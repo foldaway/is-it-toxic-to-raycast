4a7b98141875a2ff06d0d84c4a425007202ffa66
Chester How <chesterhow@gmail.com>
2022-10-08 22:38:07 +0800

    lint: fix linting errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { ActionPanel, List, Action } from "@raycast/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-type Animal ='cat' | 'dog' | 'horse'; 
+type Animal = "cat" | "dog" | "horse";
 type Plant = {
   name: string;
   commonNames: string[];
@@ -15,15 +15,17 @@ type Plant = {
 const ANIMAL_EMOJI_MAP: Record<Animal, string> = {
   cat: "🐱",
   dog: "🐶",
-  horse: "🐴"
-}
+  horse: "🐴",
+};
 
 export default function Command() {
   const [plants, setPlants] = useState<Plant[]>([]);
 
   useEffect(() => {
     async function fetchPlants() {
-      const res = await axios.get<Plant[]>("https://fourthclasshonours.github.io/toxic-plant-list-scraper/toxicPlants.json");
+      const res = await axios.get<Plant[]>(
+        "https://fourthclasshonours.github.io/toxic-plant-list-scraper/toxicPlants.json"
+      );
       setPlants(res.data);
     }
 
@@ -37,40 +39,47 @@ export default function Command() {
           key={index}
           title={plant.name}
           keywords={plant.commonNames}
-          accessories={[{
-            text: plant.toxicTo.map((animal) => ANIMAL_EMOJI_MAP[animal]).join(" ")
-          }]}
+          accessories={[
+            {
+              text: plant.toxicTo.map((animal) => ANIMAL_EMOJI_MAP[animal]).join(" "),
+            },
+          ]}
           actions={
             <ActionPanel>
               <Action.OpenInBrowser title="More details" url={plant.link} />
             </ActionPanel>
           }
           detail={
-            <List.Item.Detail metadata={
-              <List.Item.Detail.Metadata>
-                <List.Item.Detail.Metadata.TagList title="Toxic to">
-                  {plant.toxicTo.map((animal, index) => (
-                    <List.Item.Detail.Metadata.TagList.Item key={index} text={`${ANIMAL_EMOJI_MAP[animal]} ${animal[0].toUpperCase()}${animal.substring(1).toLowerCase()}`} />
+            <List.Item.Detail
+              metadata={
+                <List.Item.Detail.Metadata>
+                  <List.Item.Detail.Metadata.TagList title="Toxic to">
+                    {plant.toxicTo.map((animal, index) => (
+                      <List.Item.Detail.Metadata.TagList.Item
+                        key={index}
+                        text={`${ANIMAL_EMOJI_MAP[animal]} ${animal[0].toUpperCase()}${animal
+                          .substring(1)
+                          .toLowerCase()}`}
+                      />
                     ))}
-                </List.Item.Detail.Metadata.TagList>
-                <List.Item.Detail.Metadata.Label title="Scientific Name" text={plant.scientificName} />
-                {plant.family !== null && (
-                  <List.Item.Detail.Metadata.Label title="Family" text={plant.family} />
-                )}
-                {plant.commonNames.length > 0 && (
-                  <List.Item.Detail.Metadata.TagList title="Common Names">
-                    {plant.commonNames.map((commonName, index) => (
-                      <List.Item.Detail.Metadata.TagList.Item key={index} text={commonName} />
-                      ))}
                   </List.Item.Detail.Metadata.TagList>
-                )}
-                <List.Item.Detail.Metadata.Separator />
-                <List.Item.Detail.Metadata.Link title="Link" text="More details" target={plant.link} />
-              </List.Item.Detail.Metadata>
-            } />
+                  <List.Item.Detail.Metadata.Label title="Scientific Name" text={plant.scientificName} />
+                  {plant.family !== null && <List.Item.Detail.Metadata.Label title="Family" text={plant.family} />}
+                  {plant.commonNames.length > 0 && (
+                    <List.Item.Detail.Metadata.TagList title="Common Names">
+                      {plant.commonNames.map((commonName, index) => (
+                        <List.Item.Detail.Metadata.TagList.Item key={index} text={commonName} />
+                      ))}
+                    </List.Item.Detail.Metadata.TagList>
+                  )}
+                  <List.Item.Detail.Metadata.Separator />
+                  <List.Item.Detail.Metadata.Link title="Link" text="More details" target={plant.link} />
+                </List.Item.Detail.Metadata>
+              }
+            />
           }
         />
-        ))}
-      </List>
+      ))}
+    </List>
   );
 }
