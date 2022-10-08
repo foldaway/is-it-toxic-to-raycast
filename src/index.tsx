e22a3d1944f8a2e1e96cdce150c80f3d57fba05c
Chester How <chesterhow@gmail.com>
2022-10-08 16:02:50 +0800

    feat: support more animals

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,29 @@ import { ActionPanel, List, Action } from "@raycast/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+type Animal ='cat' | 'dog' | 'horse'; 
 type Plant = {
   name: string;
   commonNames: string[];
   scientificName: string;
   family: string | null;
   link: string;
+  toxicTo: Animal[];
 };
+
+const ANIMAL_EMOJI_MAP: Record<Animal, string> = {
+  cat: "🐱",
+  dog: "🐶",
+  horse: "🐴"
+}
 
 export default function Command() {
   const [plants, setPlants] = useState<Plant[]>([]);
 
   useEffect(() => {
     async function fetchPlants() {
-      const res = await axios.get<{ Cats: Plant[] }>("https://fourthclasshonours.github.io/toxic-plant-list-scraper/toxicPlants.json");
-      setPlants(res.data["Cats"]);
+      const res = await axios.get<Plant[]>("https://fourthclasshonours.github.io/toxic-plant-list-scraper/toxicPlants.json");
+      setPlants(res.data);
     }
 
     fetchPlants();
@@ -28,10 +36,10 @@ export default function Command() {
         <List.Item
           key={index}
           title={plant.name}
-          subtitle={plant.scientificName}
-          accessories={[
-            { text: "🐱"},
-          ]}
+          keywords={plant.commonNames}
+          accessories={[{
+            text: plant.toxicTo.map((animal) => ANIMAL_EMOJI_MAP[animal]).join(" ")
+          }]}
           actions={
             <ActionPanel>
               <Action.OpenInBrowser title="More details" url={plant.link} />
@@ -40,6 +48,11 @@ export default function Command() {
           detail={
             <List.Item.Detail metadata={
               <List.Item.Detail.Metadata>
+                <List.Item.Detail.Metadata.TagList title="Toxic to">
+                  {plant.toxicTo.map((animal, index) => (
+                    <List.Item.Detail.Metadata.TagList.Item key={index} text={`${ANIMAL_EMOJI_MAP[animal]} ${animal[0].toUpperCase()}${animal.substring(1).toLowerCase()}`} />
+                    ))}
+                </List.Item.Detail.Metadata.TagList>
                 <List.Item.Detail.Metadata.Label title="Scientific Name" text={plant.scientificName} />
                 {plant.family !== null && (
                   <List.Item.Detail.Metadata.Label title="Family" text={plant.family} />
